532bbc5149dd977ae28b39f00c980f92301de942
unknown <tuf3070@example.com>
2022-12-29 14:58:57 +0800

    install cheerio and @types/cheerio to analyze html result

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## install in this project(develop)
 ``` npm install -D ts-node  ```
+
 ``` npm install typescript -D ```
 
 ## run package.json scripts: dev
@@ -11,9 +12,12 @@
 ``` npm install superagent --save ```
 
 ## install translate file for ts
-``` npm i --save-dev @types/superagent```
+``` npm i --save-dev @types/superagent ```
+
+``` npm i --save-dev @types/cheerio ```
 
 *details below*
+
 ```https://www.npmjs.com/package/@types/superagent```
 
 
--- src/crawler.ts
+++ src/crawler.ts
@@ -1,15 +1,40 @@
 // ts->.d.ts(translate file) ->js
 // visit https://www.npmjs.com/package/node and search @types/[js file you wanna translate]
 import superagent from 'superagent' // ts cannot read js file
+import cheerio from 'cheerio'
 
+interface Course {// for array
+    title: string;
+    count: number;
+}
 
 class Crawler {
     private secret = 'secretKey';
     private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;// 勿斷行, superagent 讀不到
-    private rawHtml = '';
+    
+    getCourseInfo(html: string){
+        const $ = cheerio.load(html); // load html text
+        const courseItems = $('.course-item'); // find class named 'course-item'
+        const courseInfos: Course[]= []; // for parse result
+
+        courseItems.map((index, element)=>{ // for loop: like key value pair
+            const descs = $(element).find('.course-desc'); //find class named 'course-desc' in value
+            const title = descs.eq(0).text();// get first element and get it's text
+            const count = parseInt(descs.eq(1).text().split('：')[1], 10); // similar to uppon but parse to int
+            courseInfos.push({title, count});  // add into array
+        })
+        
+        const result = {
+            time: new Date().getTime(),
+            data: courseInfos
+        }
+        console.log(result)
+
+    }
+
     async getRawHtml(){
         const result = await superagent.get(this.url);
-        this.rawHtml = result.text;
+        this.getCourseInfo(result.text) ;
     }
     constructor(){
         this.getRawHtml();
